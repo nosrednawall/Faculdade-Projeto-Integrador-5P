3e03e9380807f6bd30b6c688c4448a9b30cd4e63
nosrednawall <nosrednawall@gmail.com>
2018-04-08 16:08:27 -0300

    Adiciondo o SetorDao e finalizado

--- fonte/src/br/org/iel/recrutaif/model/entity/Usuario.java
+++ fonte/src/br/org/iel/recrutaif/model/entity/Usuario.java
@@ -1,5 +1,69 @@
 package br.org.iel.recrutaif.model.entity;
 
-public class Usuario {
+import java.util.Calendar;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.validator.constraints.Email;
+
+@Entity
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+	
+	private String nome;
+	private Integer matricula;
+	
+	@Temporal(TemporalType.DATE)
+	private Calendar dataAdmissao;
+	
+	@Email
+	private String email;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(Integer matricula) {
+		this.matricula = matricula;
+	}
+
+	public Calendar getDataAdmissao() {
+		return dataAdmissao;
+	}
+
+	public void setDataAdmissao(Calendar dataAdmissao) {
+		this.dataAdmissao = dataAdmissao;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
---
+++ fonte/src/br/org/iel/recrutaif/testes/TestaInsereSetor.java
@@ -0,0 +1,5 @@
+package br.org.iel.recrutaif.testes;
+
+public class TestaInsereSetor {
+
+}
