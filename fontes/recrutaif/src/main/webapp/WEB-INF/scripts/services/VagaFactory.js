d77483aee2ec2520fbbc3216233496132c85f010
nosrednawall <nosrednawall@gmail.com>
2018-04-28 20:33:22 -0300

    iniciado curso de angular

--- fontes/recrutaif/src/main/webapp/WEB-INF/scripts/services/VagaFactory.js
+++
@@ -1,4 +0,0 @@
-angular.module('recrutaif').factory('VagaResource', function($resource){
-    var resource = $resource('rest/vagas/:VagaId',{VagaId:'@id'},{'queryAll':{method:'GET',isArray:true},'query':{method:'GET',isArray:false},'update':{method:'PUT'}});
-    return resource;
-});
