2f757c64302b8a325cc0d2b1fd22b73b403b533c
nosrednawall <nosrednawall@gmail.com>
2018-04-29 11:09:19 -0300

    Adicionado com sucesso a inclusaõ de novos setores +- validados

--- fontes/recrutaif/src/main/webapp/resources/js/controllers/adiciona-setor-controller.js
+++ fontes/recrutaif/src/main/webapp/resources/js/controllers/adiciona-setor-controller.js
@@ -1,13 +1,21 @@
 angular.module('recrutaif').controller('AdicionaSetorController',function($scope, $http){
     $scope.setor = {};
+    $scope.mensagem = '';
      
     $scope.submeter = function(){
-        $http.post('rest/setores/', $scope.setor)
+        if($scope.formulario.$valid){
+            $http.post('rest/setores/', $scope.setor)
             .success(function(){
-                console.log("[INFO]Setor "+ $scope.setor.nome +" Adicionado com sucesso!")
+                console.log("[INFO]Setor "+ $scope.setor.nome +" Adicionado com sucesso!");
+                $scope.mensagem = "[INFO]Setor "+ $scope.setor.nome +" Adicionado com sucesso!";
+
+                $scope.setor = '';
             })
             .error(function(){
-                console.log("[ERROR] erro ao enviar objeto ao servidor")
+                console.log("[ERROR] Não foi possível incluir setor");
+                $scope.mensagem = "[ERROR] Não foi possível incluir setor";
+                
             });
+        }
     };
 });
