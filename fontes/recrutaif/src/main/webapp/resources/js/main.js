3a9896d09675ab7bf6ec260d2af2cd22b618d2ca
nosrednawall <nosrednawall@gmail.com>
2018-04-29 08:52:41 -0300

    Adicionado sistema de rotas automáticas pelo #

--- fontes/recrutaif/src/main/webapp/resources/js/main.js
+++ fontes/recrutaif/src/main/webapp/resources/js/main.js
@@ -1,7 +1,19 @@
 angular.module('recrutaif', ['minhasDiretivas','ngAnimate','ngRoute'])
 .config(function($routeProvider){
+
+    //oculta o # dos enderecos, porém o servidor não está preparado
+    // $locationProvider.html5Mode(true);
+
     $routeProvider.when('/setores',{
         templateUrl: 'resources/partials/principal.html',
         controller: 'SetorController'
     });
+
+    $routeProvider.when('/setores/new',{
+        templateUrl: 'resources/partials/principal.html',
+        controller: 'SetorController'
+    });
+    
+    //caso o usuario digite algum caminho que não exista ele será redirecionado
+    $routeProvider.otherwise({ redirectTo: '/setores'});
 }); //criado módulo chamado recrutaif que não tem nenhuma dependencia ainda, não depende de outros módulos
