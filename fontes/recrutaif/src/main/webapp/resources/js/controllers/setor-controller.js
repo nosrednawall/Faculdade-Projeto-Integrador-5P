25d204527dc1d1093d49f4c065b27cb597a6ab3a
nosrednawall <nosrednawall@gmail.com>
2018-04-28 22:48:04 -0300

    Finalmente consegui fazer um select via rest e angular

--- fontes/recrutaif/src/main/webapp/resources/js/controllers/setor-controller.js
+++ fontes/recrutaif/src/main/webapp/resources/js/controllers/setor-controller.js
@@ -1,7 +1,27 @@
-angular.module('recrutaif').controller('SetorController',function($scope){
+angular.module('recrutaif').controller('SetorController',function($scope,$http){
     
-    $scope.foto = {
-        titulo : 'Leão',
-        url : 'http://i.huffpost.com/gen/2841234/thumbs/o-LION-900.jpg?7'
-    }
+    // $scope.fotos = [
+    //     {
+    //         titulo : 'Leão',
+    //         url : 'http://i.huffpost.com/gen/2841234/thumbs/o-LION-900.jpg?7'
+    //     },
+    //     {
+    //         titulo : 'Leão 2',
+    //         url : 'http://i.huffpost.com/gen/2841234/thumbs/o-LION-900.jpg?7'
+    //     },
+    //     {
+    //         titulo : 'Leão 3',
+    //         url : 'http://i.huffpost.com/gen/2841234/thumbs/o-LION-900.jpg?7'
+    //     }
+    // ];
+
+    $scope.setores = [];
+
+    var promise= $http.get('rest/setores/');
+
+    promise.then(function(retorno){
+        $scope.setores = retorno.data;
+    }).catch(function(error){
+        console.log("error");
+    });
 });
