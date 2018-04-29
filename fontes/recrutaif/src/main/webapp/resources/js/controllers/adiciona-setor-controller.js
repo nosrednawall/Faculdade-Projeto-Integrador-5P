31f75e54d458ba488ef8fc2ff8244005bbf7d9d4
nosrednawall <nosrednawall@gmail.com>
2018-04-29 10:49:40 -0300

    método post criado com sucesso !

--- fontes/recrutaif/src/main/webapp/resources/js/controllers/adiciona-setor-controller.js
+++ fontes/recrutaif/src/main/webapp/resources/js/controllers/adiciona-setor-controller.js
@@ -1,7 +1,13 @@
-angular.module('recrutaif').controller('AdicionaSetorController',function($scope){
+angular.module('recrutaif').controller('AdicionaSetorController',function($scope, $http){
     $scope.setor = {};
      
     $scope.submeter = function(){
-        console.log($scope.setor);
+        $http.post('rest/setores/', $scope.setor)
+            .success(function(){
+                console.log("[INFO]Setor "+ $scope.setor.nome +" Adicionado com sucesso!")
+            })
+            .error(function(){
+                console.log("[ERROR] erro ao enviar objeto ao servidor")
+            });
     };
 });
