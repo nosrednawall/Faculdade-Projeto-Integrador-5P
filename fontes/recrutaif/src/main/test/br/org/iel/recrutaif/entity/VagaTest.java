291933504b1d1d318c24aef411c271e6d8adf656
evertonsausen <everton.sausen@hotmail.com>
2018-05-09 23:36:07 -0300

    Cronograma atualizado

--- fontes/recrutaif/src/main/test/br/org/iel/recrutaif/entity/VagaTest.java
+++ fontes/recrutaif/src/main/test/br/org/iel/recrutaif/entity/VagaTest.java
@@ -1,31 +1,44 @@
 package br.org.iel.recrutaif.entity;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import java.io.ByteArrayInputStream;
 import java.text.ParseException;
 import org.junit.jupiter.api.Test;
+
 public class VagaTest {
 
-//*//**
-//* Titulo:
-//* 1-Não deve ficar em branco 
-//* 2-Não ultrapassar 50 caracteres
-//* 3-Não deve conter numeros
-//*//*	
-	
+	// *//**
+	// * Titulo:
+	// * 1-Não deve ficar em branco
+	// * 2-Não ultrapassar 50 caracteres
+	// * 3-Não deve conter numeros
+	// *//*
+
 	@Test
 	public void deveRetornarErroSeOCampoEstiverVazio() throws ParseException {
-		
+
 		String conteudo = "";
 		Vaga vagaTeste = new Vaga();
-		
+
 		vagaTeste.setTitulo(conteudo);
 
-//		Collection<titulo> Titulo  = importador.(input);
+		// Collection<titulo> Titulo = importador.(input);
 		assertTrue("O campo está vazio", vagaTeste.getTitulo().isEmpty());
 
-}
+	}
 
+	@Test
+	public void naodeveRetornarErroSeOCampoEstiverPreenchido() throws ParseException {
 
+		String conteudo = "hjgjh";
+		Vaga vagaTeste = new Vaga();
+
+		vagaTeste.setTitulo(conteudo);
+
+		// Collection<titulo> Titulo = importador.(input);
+		assertTrue("O campo está preenchido", !vagaTeste.getTitulo().isEmpty());
+
+	}
 
 }
