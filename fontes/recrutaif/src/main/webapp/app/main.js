45b19129870f2b0c8a74dc2111a3593ca2e0ce5b
nosrednawall <nosrednawall@gmail.com>
2018-05-15 19:19:41 -0300

    Corrigido as alteações de pastas nos arquivos main e index

--- fontes/recrutaif/src/main/webapp/app/main.js
+++ fontes/recrutaif/src/main/webapp/app/main.js
@@ -9,27 +9,27 @@ angular.module('recrutaif', ['minhasDiretivas','ngAnimate','ngRoute','ngResource
 
     //configurração de rota, sempre a rota, o seu template e o controller correspondente
     $routeProvider.when('/setores',{
-        templateUrl: 'resources/js/angular/setor/setores.html',
+        templateUrl: 'app/setor/setores.html',
         controller: 'SetoresController'
     });
 
     $routeProvider.when('/setor/new',{
-        templateUrl: 'resources/js/angular/setor/setor.html',
+        templateUrl: 'app/setor/setor.html',
         controller: 'SetorController'
     });
     
     $routeProvider.when('/setor/edit/:setorId',{
-        templateUrl: 'resources/js/angular/setor/setor.html',
+        templateUrl: 'app/setor/setor.html',
         controller: 'SetorController'
     });
     
     $routeProvider.when('/login', {
-        templateUrl: 'resources/js/angular/login/login.html',
+        templateUrl: 'app/login/login.html',
         controller: 'LoginController'
     });
 
     $routeProvider.when('/principal', {
-        templateUrl: 'resources/js/angular/principal/principal.html',
+        templateUrl: 'app/principal/principal.html',
     });
 
     $routeProvider.otherwise({ redirectTo: '/principal'});
