3ba2834382a533a0d9d16851d53164b16eed5edf
nosrednawall <nosrednawall@gmail.com>
2018-05-26 10:53:37 -0300

    adicionado valores ocultos em usuario

--- fontes/recrutaif/src/main/java/br/org/iel/recrutaif/controllers/rests/UsuarioRest.java
+++ fontes/recrutaif/src/main/java/br/org/iel/recrutaif/controllers/rests/UsuarioRest.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.json.Json;
 import javax.persistence.NoResultException;
 import javax.persistence.OptimisticLockException;
 import javax.ws.rs.Consumes;
@@ -13,9 +14,12 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriBuilder;
+
+import com.google.gson.Gson;
 
 import br.org.iel.recrutaif.model.dao.UsuarioDao;
 import br.org.iel.recrutaif.model.entity.Usuario;
@@ -31,18 +35,36 @@ public class UsuarioRest {
 
 	// método para criar um usuario
 	@POST
-	@Consumes("application/json")
-	public Response create(Usuario entity) {
-		System.out.println(entity.getNome());
-		System.out.println(entity.getEmail());
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response create(String usuarioGson) {
+
+		Gson gson = new Gson();
 		
-//Conversamos sobre um if para testar via controlers
+		Usuario entity = gson.fromJson(usuarioGson, Usuario.class);	
 		
-		dao.save(entity);
-		return Response
-				.created(UriBuilder.fromResource(UsuarioRest.class).path(String.valueOf(entity.getId())).build())
-				.build();
+		System.out.println(entity);
+		// Conversamos sobre um if para testar via controlers
+
+//		dao.save(entity);
+//		return Response.created(UriBuilder.fromResource(UsuarioRest.class).path(String.valueOf(entity.getId())).build())
+//				.build();
+		
+		return Response.ok().build();
 	}
+	
+	// @POST
+	// @Consumes(MediaType.APPLICATION_JSON)
+	// public Response create(Usuario entity) {
+	// System.out.println(entity.getNome());
+	// System.out.println(entity.getEmail());
+	//
+	//// Conversamos sobre um if para testar via controlers
+	//
+	// dao.save(entity);
+	// return Response
+	// .created(UriBuilder.fromResource(UsuarioRest.class).path(String.valueOf(entity.getId())).build())
+	// .build();
+	// }
 
 	@GET
 	@Path("/{id:[0-9][0-9]*}")
@@ -64,18 +86,18 @@ public class UsuarioRest {
 	@Produces("application/json")
 	public List<Usuario> listaUsuarios() {
 		final List<Usuario> results = dao.listaTodos();
-			
+
 		return results;
 	}
 
-//	@GET
-//	@Produces("application/json")
-//	public List<Usuario> listaUsuarios(@QueryParam("status") StatusUsuario status) {
-//		final List<Usuario> results = dao.listaTodos(status);
-//		return results;
-//	}
+	// @GET
+	// @Produces("application/json")
+	// public List<Usuario> listaUsuarios(@QueryParam("status") StatusUsuario
+	// status) {
+	// final List<Usuario> results = dao.listaTodos(status);
+	// return results;
+	// }
 
-	
 	@PUT
 	@Path("/{id:[0-9][0-9]*}")
 	@Consumes("application/json")
@@ -93,14 +115,14 @@ public class UsuarioRest {
 			return Response.status(Status.NOT_FOUND).build();
 		}
 		try {
-			
+
 			System.out.println(entity.getNome());
 			System.out.println(entity.getEmail());
 			System.out.println(entity.getMatricula());
 			System.out.println(entity.getSenha());
-			
+
 			entity = dao.update(entity);
-			
+
 			System.out.println("depois");
 		} catch (OptimisticLockException e) {
 			return Response.status(Response.Status.CONFLICT).entity(e.getEntity()).build();
