fb6163a27aa78e64a3edc29e0773c2663d5e0117
nosrednawall <nosrednawall@gmail.com>
2018-05-31 11:21:00 -0300

    Adicionado com sucesso use strict nos arquivos angulars gerenciados pelo anderson

--- fontes/recrutaif/src/main/webapp/app/login/login-controller.js
+++ fontes/recrutaif/src/main/webapp/app/login/login-controller.js
@@ -1,41 +1,46 @@
-angular.module('recrutaif').controller('LoginController',
-    function ($cookieStore, $window, $q, $scope, $http, $location) {
+(function () {
+    'use strict';
 
-        $scope.usuario = {};
-        $scope.mensagem = '';
+    angular.module('recrutaif').controller('LoginController',
+        function ($cookieStore, $window, $q, $scope, $http, $location) {
 
-        delete $window.sessionStorage.token;
+            $scope.usuario = {};
+            $scope.mensagem = '';
 
-
-        $scope.autenticar = function () {
-
-            var usuarioNaoLogado = $scope.usuario;
-
-            $http.post('rest/login', { email: usuarioNaoLogado.email, senha: usuarioNaoLogado.senha })
-                .then(function (usuarioLogado) {
+            delete $window.sessionStorage.token;
 
 
-                    console.log(usuarioLogado);
+            $scope.autenticar = function () {
 
-                    $cookieStore.put('nome', usuarioLogado.data.nome);
-                    console.log($cookieStore.get('nome'))
-                    $cookieStore.put('id', usuarioLogado.data.id);
-                    $cookieStore.put('permissao', usuarioLogado.data.permissao);
-                    $cookieStore.put('logado', true);
+                var usuarioNaoLogado = $scope.usuario;
 
-                    $location.path('/principal');
-
-                    // { "id": 3, "nome": "Ghost", "senha": "ghost", "matricula": "1011", "email": "ghost@ghost", "dataAdmissao": { "year": 2018, "month": 4, "dayOfMonth": 17, "hourOfDay": 0, "minute": 0, "second": 0 }, "permissao": "ADMINISTRADOR", "status": "ATIVO", "candidaturas": [] }
+                $http.post('rest/login', { email: usuarioNaoLogado.email, senha: usuarioNaoLogado.senha })
+                    .then(function (usuarioLogado) {
 
 
-                }, function (erro) {
-                    console.log('Esse é o erro de login ' + erro);
-                    console.log('Entrou em deu errado')
+                        console.log(usuarioLogado);
 
-                    $scope.usuario = {};
-                    $scope.mensagem = 'Login ou senha inválidos!';
-                }
-                );
-        };
+                        $cookieStore.put('nome', usuarioLogado.data.nome);
+                        console.log($cookieStore.get('nome'))
+                        $cookieStore.put('id', usuarioLogado.data.id);
+                        $cookieStore.put('permissao', usuarioLogado.data.permissao);
+                        $cookieStore.put('logado', true);
 
-    });
+                        $location.path('/principal');
+
+                        // { "id": 3, "nome": "Ghost", "senha": "ghost", "matricula": "1011", "email": "ghost@ghost", "dataAdmissao": { "year": 2018, "month": 4, "dayOfMonth": 17, "hourOfDay": 0, "minute": 0, "second": 0 }, "permissao": "ADMINISTRADOR", "status": "ATIVO", "candidaturas": [] }
+
+
+                    }, function (erro) {
+                        console.log('Esse é o erro de login ' + erro);
+                        console.log('Entrou em deu errado')
+
+                        $scope.usuario = {};
+                        $scope.mensagem = 'Login ou senha inválidos!';
+                    }
+                    );
+            };
+
+        });
+
+})();
