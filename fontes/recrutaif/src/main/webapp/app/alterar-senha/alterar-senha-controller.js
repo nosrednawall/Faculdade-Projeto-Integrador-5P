11f8b6e4b8c6264bcf0dcf8e584bfc5973392813
nosrednawall <nosrednawall@gmail.com>
2018-06-03 12:12:50 -0300

    erro em alterar senha, tenho que perguntar ao professor

--- fontes/recrutaif/src/main/webapp/app/alterar-senha/alterar-senha-controller.js
+++ fontes/recrutaif/src/main/webapp/app/alterar-senha/alterar-senha-controller.js
@@ -1,6 +1,6 @@
 (function () {
     'use strict';
-    angular.module('recrutaif').controller('AlterarSenhaController', function ($cookieStore, $rootScope, $scope, recursoUsuario) {
+    angular.module('recrutaif').controller('AlterarSenhaController', function ($cookieStore, $rootScope, $scope, $resource,recursoUsuario) {
         //módulos que não são mais utilizados, porque estão sendo injetados pelo recursoSetor: $http,$resource
 
         //módulo controller, para setor
@@ -9,26 +9,23 @@
         $scope.filtro = ''; //variável responsável pelo filtro de setores, dentro do scope
         $scope.mensagem = '';   //variável responsável pela mensagem de interação com o usuário dentro do scope
         $scope.usuario = [];
-        var User = $resource('rest/usuarios/:usuarioId', { usuarioId: '@id' });
 
-        User.get({ userId: 123 }, function (u, getResponseHeaders) {
-            u.abc = true;
-            u.$save(function (u, putResponseHeaders) {
-                //u => saved user object
-                //putResponseHeaders => $http header getter
-            });
+
+        var User = $resource('rest/usuarios/:usuarioId', {userId:'@id'});
+        var user = User.get({userId:$cookieStore.get('id')}, function(u, getResponseHeaders) {
+          
+          console.log(user);
+        //   user.$save(function(u, putResponseHeaders){
+        //       $scope.usuario = u;
+        //   });
         });
 
-        console.log($cookieStore.get('id')); //deu certo aqui
-
-        // essa variável não é mais utilizada porque está sendo injetada pelo recursoSetor nos módulos acima
-        // var recursoSetor = $resource('rest/setores/:setorId');
-
         //função busca uma lista de setores
-        // recursoUsuario.query({ id: $cookieStore.get('id') }, function (usuario) {
-        //     console.log(usuario);
-        //     //salva a lista de setores dentro da variável de escope $setores
-        //     // $scope.usuario = usuario;
+        // recursoUsuario.query({ id: $cookieStore.get('id') }, function (usuarios) {
+        // recursoUsuario.({ id: $cookieStore.get('id') }, function (usuarios) {
+
+
+
         // }, function (erro) {
         //     //caso dê erro imprime o erro para o usuário
         //     console.log(error);
