934c39122d4ea008919f96be22afb7b39f40be66
evertonsausen <everton.sausen@hotmail.com>
2018-06-03 09:28:59 -0300

    Mudança nos pacotes

---
+++ fontes/recrutaif/src/main/test/br/org/iel/recrutaif/controllers/validators/regras/DataValidaIntervaloTest.java
@@ -0,0 +1,14 @@
+package br.org.iel.recrutaif.controllers.validators.regras;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DataValidaIntervaloTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
